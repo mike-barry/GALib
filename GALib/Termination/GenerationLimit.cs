e05d7ea5030f7fa02d694f4d531d5225ff12ee72
Mike Barry <mike@barry.net>
2019-04-01 20:45:34 -0500

    Initial implementation of TerminationMethod

---
+++ GALib/Termination/GenerationLimit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GALib.Termination
+{
+  public class GenerationLimit : TerminationMethod
+  {
+    private int maxGenerations = 100;
+
+    public int MaxGenerations
+    {
+      get
+      {
+        return maxGenerations;
+      }
+      set
+      {
+        if (value < 1)
+          maxGenerations = value;
+        else
+          maxGenerations = value;
+      }
+    }
+
+    public override bool CheckTermination(IGeneticAlgorithm ga)
+    {
+      if (ga.GenerationNumber >= maxGenerations)
+        return true;
+      else
+        return false;
+    }
+  }
+}
---
+++ GALib/Termination/TerminationMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GALib.Termination
+{
+  public abstract class TerminationMethod
+  {
+    public abstract bool CheckTermination(IGeneticAlgorithm ga);
+  }
+}
