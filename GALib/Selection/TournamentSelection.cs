af72618a34f9a177ad20d40fefca1b7d4f55cfb2
Mike Barry <mike@barry.net>
2019-04-12 22:08:35 -0500

    Implemented tournament selection

--- GALib/Selection/TournamentSelection.cs
+++ GALib/Selection/TournamentSelection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace GALib.Selection
@@ -12,13 +13,77 @@ namespace GALib.Selection
   /// </remarks>
   public class TournamentSelection : SelectionMethod
   {
+    private int tournamentSize = 5;
+
+    /// <summary>
+    /// Gets or sets the size of the tournament.
+    /// </summary>
+    /// <value>
+    /// The size of the tournament.
+    /// </value>
+    [Category("Parameters"), DisplayName("Tournament Size")]
+    public int TournamentSize
+    {
+      get
+      {
+        return tournamentSize;
+      }
+      set
+      {
+        if (value < 1)
+          tournamentSize = 1;
+        else
+          tournamentSize = value;
+      }
+    }
+
     /// <summary>
     /// Performs tournament selection
     /// </summary>
     /// <returns></returns>
     public override List<IGenotype> DoSelection()
     {
-      throw new NotImplementedException();
+      int index;
+      IGenotype best;
+      ICollection<IGenotype> selection;
+
+      if (AllowDuplicates)
+        selection = new List<IGenotype>(SelectionCount);
+      else
+        selection = new SafeHashSet<IGenotype>(MaxRetriesForDuplicates);
+
+      while (selection.Count < SelectionCount)
+      {
+        index = Tools.StaticRandom.Next(0, Population.Count);
+        best = Population[index];
+
+        for (int i = 1; i < TournamentSize; i++)
+        {
+          index = Tools.StaticRandom.Next(0, Population.Count);
+
+          if (best.Fitness < Population[index].Fitness)
+            best = Population[index];
+        }
+
+        selection.Add(best);
+      }
+
+      if (AllowDuplicates)
+        return (List<IGenotype>)selection;
+      else
+        return selection.ToList();
+    }
+
+
+    /// <summary>
+    /// Converts to string.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="System.String" /> that represents this instance.
+    /// </returns>
+    public override string ToString()
+    {
+      return "Tournament Selection";
     }
   }
 }
